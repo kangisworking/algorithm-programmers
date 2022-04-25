325e9c07d267861b2e8b7486b64f37f49a9bd9ba
Hyunseo Kang <hyunseo@marvrus.com>
2022-04-25 09:39:20 +0900

    크레인 인형뽑기

--- crane_toy/index.js
+++ crane_toy/index.js
@@ -23,35 +23,6 @@ function solution(board, moves) {
   return answer;
 }
 
-// function solution(board, moves) {
-//   let answer = 0;
-//   let moved = [];
-
-//   for (let i = 0; i < moves.length; i++) {
-//     for (let j = 0; j < board.length; j++) {
-//       if (board[j][moves[i] - 1] !== 0) {
-//         console.log("board[j][moves[i] - 1] :>> ", board[j][moves[i] - 1]);
-//         moved.push(board[j][moves[i] - 1]);
-//         board[j][moves[i] - 1] = 0;
-//         break;
-//       }
-//     }
-//   }
-//   //   console.log("moved :>> ", moved);
-//   for (let i = 0; i < moved.length; i++) {
-//     console.log("moved :>> ", moved);
-//     console.log("moved[i] :>> ", moved[i]);
-//     console.log("moved[i+1] :>> ", moved[i + 1]);
-//     if (moved[i] === moved[i + 1]) {
-//       answer += 2;
-//       moved.splice(i, 2);
-//       i = -1;
-//     }
-//   }
-
-//   return answer;
-// }
-
 let input = [
   [
     [0, 0, 0, 0, 0],
